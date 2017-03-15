89f1666d0fa924e63758bbe071c14bee0a2e6902
hisyam22 <addisyam22@gmail.com>
2017-03-15 10:23:39 +0700

    firstgit

---
+++ app/src/main/java/com/example/hisyam/cobagit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hisyam.cobagit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int x = 5;
+
+    }
+}
